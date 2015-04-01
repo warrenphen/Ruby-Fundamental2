e0a9c47504ffc697714d645fab21afe459829814
Warren Phen <warren.phen@gmail.com>
2015-04-01 15:47:30 -0400

    created function for display groceries and added rice to the list

--- exercise6.rb
+++ exercise6.rb
@@ -1,8 +1,13 @@
+
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
+def display(list)
+	list.each { |item| puts "* #{item}" }
+end
 
-grocery_list.each { |item| puts "* #{item}" }
+grocery_list << "rice"
 
+display(grocery_list)
 
 
 
