790b2f2788e4d8cf5a52abe3ea680ba2864352c4
Warren Phen <warren.phen@gmail.com>
2015-04-01 16:16:16 -0400

    added ability to look for bananas in list

--- exercise6.rb
+++ exercise6.rb
@@ -11,5 +11,17 @@ display(grocery_list)
 
 puts grocery_list.length
 
+if grocery_list.include?("bananas")
+	puts "You don't need to pick up bananas!"
+else
+	puts "You need to pick up bananas!"
+end
+
+
+
+
+	
+
+
 
 
